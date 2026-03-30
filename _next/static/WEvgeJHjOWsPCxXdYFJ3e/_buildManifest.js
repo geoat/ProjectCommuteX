7aec5f96c80a833fecb52c8da5d6bf62dce32fb9
Thomas Arun Geo (DC/EMX3) <arungeo.thomas@boschrexroth.nl>
2026-03-30 18:19:23 +0200

    Dummy

---
+++ _next/static/WEvgeJHjOWsPCxXdYFJ3e/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/ProjectCommuteX//_next/:path+",
+        "destination": "/ProjectCommuteX/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
